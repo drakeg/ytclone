5db1d94fb46a9bd86f9cab06e1e9f2f75e0b2dfb
Greg Drake <greg@madmallards.com>
2026-07-23 22:32:38 -0400

    Make SQLite database path configurable

--- yt/settings.py
+++ yt/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "yt.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": Path(os.getenv("DJANGO_DB_PATH", BASE_DIR / "db.sqlite3")),
     }
 }
 

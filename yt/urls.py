ed8d26055cfe4e6a69794161614c3e1bb6d1d1a7
Greg Drake <greg@madmallards.com>
2026-08-16 23:46:44 -0400

    Add current user profile route

--- yt/urls.py
+++ yt/urls.py
@@ -8,9 +8,6 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
@@ -20,6 +17,7 @@ from django.urls import include, path, reverse_lazy
 from django.views.generic import RedirectView
 from django.views.generic.edit import CreateView
 
+from video.account_views import current_profile
 from .forms import RegistrationForm
 
 urlpatterns = [
@@ -34,6 +32,7 @@ urlpatterns = [
         ),
         name='register',
     ),
+    path('accounts/profile/', current_profile, name='current_profile'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('videos/', include('video.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

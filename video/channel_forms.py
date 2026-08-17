fd5bb3cb12076b5a697e8f053d4d3e5a8cc3b9ef
Greg Drake <greg@madmallards.com>
2026-08-16 23:46:13 -0400

    Add channel creation form

---
+++ video/channel_forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Channel
+
+
+class ChannelForm(forms.ModelForm):
+    class Meta:
+        model = Channel
+        fields = ["name", "description", "thumbnail"]

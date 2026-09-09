86b4dd8da75fea007d8927419ea21ec162386c18
Greg Drake <greg@madmallards.com>
2026-09-09 10:29:50 -0400

    feat: add structured tag discovery view

--- video/metadata_views.py
+++ video/metadata_views.py
@@ -1,19 +1,31 @@
 from django.shortcuts import get_object_or_404, render
 
-from .metadata_models import Hashtag
+from .metadata_models import Hashtag, Tag
 from .models import Video
+
+
+def _topic_videos(request, relation, topic):
+    return (
+        Video.objects.visible_to(request.user)
+        .filter(**{relation: topic})
+        .select_related("author", "channel", "category")
+        .order_by("-pub_date", "-pk")
+    )
+
+
+def tag_detail(request, name):
+    tag = get_object_or_404(Tag, name=name.lower())
+    return render(
+        request,
+        "videos/tag_detail.html",
+        {"tag": tag, "videos": _topic_videos(request, "tags", tag)},
+    )
 
 
 def hashtag_detail(request, name):
     hashtag = get_object_or_404(Hashtag, name=name.lower())
-    videos = (
-        Video.objects.visible_to(request.user)
-        .filter(hashtags=hashtag)
-        .select_related("author", "channel", "category")
-        .order_by("-pub_date", "-pk")
-    )
     return render(
         request,
         "videos/hashtag_detail.html",
-        {"hashtag": hashtag, "videos": videos},
+        {"hashtag": hashtag, "videos": _topic_videos(request, "hashtags", hashtag)},
     )

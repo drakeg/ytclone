2e27e1359da25f51ae54e95ce1c9d825f1adfc78
Greg Drake <greg@madmallards.com>
2026-09-13 21:34:25 -0400

    fix: preserve notification page safely

--- video/notification_views.py
+++ video/notification_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.utils import timezone
 from django.views.decorators.http import require_POST
 
@@ -37,7 +38,7 @@ def notification_mark_read(request, pk):
 
     page = request.POST.get("page")
     if page:
-        return redirect(f"{request.path.rsplit('/', 3)[0]}/?page={page}")
+        return redirect(f"{reverse('notification_list')}?page={page}")
     return redirect("notification_list")
 
 

6464282b748ce3b632382d251e93d5d9027be1e1
Greg Drake <greg@madmallards.com>
2026-09-24 07:19:10 -0400

    refactor: remove legacy notification handlers

--- video/views.py
+++ video/views.py
@@ -153,37 +153,6 @@ def channel_analytics(request, pk):
     )
 
 
-@login_required
-def notification_list(request):
-    notifications = request.user.notifications.select_related(
-        "actor", "video", "channel"
-    )
-    return render(
-        request,
-        "videos/notification_list.html",
-        {"notifications": notifications},
-    )
-
-
-@login_required
-@require_POST
-def notification_mark_read(request, pk):
-    notification = get_object_or_404(Notification, pk=pk, recipient=request.user)
-    if notification.read_at is None:
-        notification.read_at = timezone.now()
-        notification.save(update_fields=["read_at"])
-    return redirect("notification_list")
-
-
-@login_required
-@require_POST
-def notification_mark_all_read(request):
-    request.user.notifications.filter(read_at__isnull=True).update(
-        read_at=timezone.now()
-    )
-    return redirect("notification_list")
-
-
 @ensure_csrf_cookie
 def video_detail(request, pk):
     video = get_object_or_404(Video.objects.visible_to(request.user), pk=pk)
